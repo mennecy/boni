edd54049161357be087b2af96cc041dd04a12d3d
meb <mennecy.echange.boni@gmail.com>
2021-03-22 14:18:21 +0100

    Tring to recover the git push

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # echo "# boni" >> README.md
 # git init
 # git add README.md
+# git add .
 # git commit -m "first commit"
 # git branch -M main
 # git remote add origin https://github.com/mennecy/boni.git
